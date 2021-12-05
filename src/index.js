cb184bcc3eda54a4fccc3dde0de7a8e892abd1a1
alich6 <alicheraghi148@gmail.com>
2021-12-05 19:36:38 +0330

    cache bug fixed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer } from 'react-toastify';
-import { ReactPwa } from "react-pwa-app";
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { MainStyle } from './common/styles/MainStyles';
 import { theme } from './common/styles/theme';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
 import { configureStore } from './redux/configStore';
 import Spinner from './components/Spinner/Spinner';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -24,35 +24,25 @@ export const select = s => s(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactPwa
-      test //is to install in localhost, not required
-      config={{
-        swUrl: "/service-worker.js", // sw file in public
-        onUpdate: (reg) => {
-          console.log(reg);
-        },
-        onSuccess: (reg) => {
-          console.log(reg);
-        },
-      }}
-    >
-
-      <ThemeProvider theme={theme}>
-        <PersistGate loading={<Spinner size='small' />} persistor={persistor}>
-          <Provider store={store}>
-            <MainStyle />
-            <BrowserRouter>
-              <App />
-              <ToastContainer rtl />
-            </BrowserRouter>
-          </Provider>
-        </PersistGate>
-      </ThemeProvider>
-    </ReactPwa>
-
+    <ThemeProvider theme={theme}>
+      <PersistGate loading={<Spinner size='small' />} persistor={persistor}>
+        <Provider store={store}>
+          <MainStyle />
+          <BrowserRouter>
+            <App />
+            <ToastContainer rtl />
+          </BrowserRouter>
+        </Provider>
+      </PersistGate>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
